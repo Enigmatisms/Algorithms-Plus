d743b95e1427ca1b905985e8b45becada3a04114
Enigmatisms <984041003@qq.com>
2020-08-21 15:30:04 +0800

    "DP fundamental learning."

--- py/dp.py
+++ py/dp.py
@@ -16,7 +16,8 @@ def naiveBackPack(weights, values, capc, item):
     if capc <= 0 or item <= 0:
         return 0
     else:
-        if weights[item] > capc:
+        # The initial case is that, item is the total number of items. Therefore -1 index is valid
+        if weights[item - 1] > capc:
             # if capc too small, change another item by index --
             return naiveBackPack(weights, values, capc, item - 1)
         else:
@@ -26,39 +27,32 @@ def naiveBackPack(weights, values, capc, item):
             ## move on to the next item
             return max(
                 naiveBackPack(weights, values, capc, item - 1),
-                naiveBackPack(weights, values, capc - weights[item], item - 1) + values[item]
+                naiveBackPack(weights, values, capc - weights[item - 1], item - 1) + values[item - 1]
             )
 
-# non recursive version
-# I think, the integer feature is essential
-# In the recursive version, we implicitly accumulate the values
-# The accumulated value is propagated during return and function stacks
-# But within one function, we need storage media
-# notice that, there could be sparsity in weights
-# e.g weights = [0, 50, 100], capc = 120
-# If we use capc to represent the whole space, it would be a waste
-# We want to directly use the number of all the possible combinations
-# Then we don't have to use a list but a hash table
+# non recursive
 def backPack(weights, values, capc, N):
-    min_val = min(weights)
-    total = np.zeros((itemN, capc - min_val))
-    # traverse N items
+    min_wt = min(weights)
+    total = np.zeros((N, capc + 1))        # assume K is set
+    
     for i in range(N):
-        # traverse possible weights (decreasingly)
-        for wt in range(capc, min_val - 1, -1):
-            # adding ith item will surpass the limit
-            if capc < wt + weights[i]:
-                total[i][wt - min_val] = total[i - 1][wt - min_val]
-            else:
-                total[i][wt + weights[i] - min_val] = max(
-                    total[i - 1][wt - min_val] + values[i],
-                    total[i - 1][wt - min_val]
+        for wt in range(0, capc + 1):          # wt is the backpack current maximum weight
+            if weights[i] <= wt:
+                if i == 0 or wt == 0:
+                    total[0][wt] = values[i]
+                total[i][wt] = max(
+                    total[i - 1][wt],
+                    total[i - 1][wt - weights[i]] + values[i]
                 )
+            else:
+                total[i][wt] = total[i - 1][wt]
+    print(total)
+    return np.max(total[-1, :])
 
 if __name__ == "__main__":
     capc = 10
     N = 3
-    wts = [3, 4, 5]
+    wts = np.array([3, 4, 5])
     vals = [4, 5, 6]
     print("Naive:", naiveBackPack(wts, vals, capc, N))
     print("Non-recursive:", backPack(wts, vals, capc, N))
