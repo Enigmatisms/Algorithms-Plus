67d6b577b66195b41e94f6a39f1f652a55e46594
Enigmatisms <984041003@qq.com>
2021-06-10 23:17:27 +0800

    SDF partly built (single mesh piece)

--- cpp/SDF/include/SDF.hpp
+++ cpp/SDF/include/SDF.hpp
@@ -7,16 +7,43 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <iostream>
-#include <boost/function.hpp>
+#include <unordered_map>
 
 /** 基于SDF的Mesh合并方法实现 */
 
-typedef std::vector<Eigen::Vector2d> Mesh;
+typedef struct Edge {
+    Eigen::Vector2d sp;
+    Eigen::Vector2d ep;
+    double weight;
+};
+
+typedef std::vector<Eigen::Vector3d> Mesh;
 typedef Eigen::Matrix<uchar, -1, -1> MatrixXu;
+typedef std::vector<Edge> Edges;
+
+struct hashFunctor {
+    template<typename T>
+    uint64_t operator() (const std::pair<T, T>& pr) const {
+        uint64_t h1 = std::hash<T>{}(pr.first);
+        uint64_t h2 = std::hash<T>{}(pr.second);
+        return h1 ^ h2;
+    }
+};
+
+struct equalFunctor {
+    template<typename T>
+    bool operator() (const std::pair<T, T>& p1, const std::pair<T, T>& p2) const {
+        return (p1.first == p2.first) && (p1.second == p2.second);
+    }
+};
+
+typedef std::unordered_map<std::pair<int, int>, Edges, hashFunctor, equalFunctor> EdgeMap;
+
 
 const double grid_size = 0.1;           // Marching Square使用的grid大小
 
 class SDF {
+using Vertex = std::vector<std::pair<uchar, uchar>>;
 public:
     SDF() {}
     ~SDF() {}
@@ -25,98 +52,66 @@ public:
      * @brief 将两个待匹配的Mesh限制在一个有裕量的BoundingBox里面
      * @param m1m2 是两个Mesh （为了方便起见，只考虑两个mesh具有重合的部分）
      * @param t1br boundingBox的左上和右下点, tl = (w, x), br = (y, z) 
-     * @note 对于实际的两个mesh（可能分段）也比较容易实现，只需要划分grid就好
+     * @note 对于实际的两个mesh（能分段）也比较容易实现，只需要划分grid就好
      */
-    void meshBoundingBox(const Mesh& m1, const Mesh& m2, Eigen::Vector4d& tlbr) const;
-
+    void meshBoundingBox(const Edges& m1, const Edges& m2, Eigen::Vector2d& tl, Eigen::Vector2i& rnc) const;
 private:
+    void singleMeshSDF(const Edges& m, const Eigen::Vector2d& tl, const Eigen::Vector2i& grid_size,
+        Eigen::MatrixXd& sdf, Eigen::MatrixXd& alpha, Eigen::MatrixXi& idx) const;
     /**
      * @brief 计算出来的应该是 每个Grid上的SDF值，这样会方便之后的Marching Squares计算
-     * 
      */
-    void singleMeshSDF(const Mesh& mesh, const Eigen::Vector4d& tlbr, Eigen::MatrixXd& sdf) const ;
+    void doubleMeshSDF(const Edges& m1, const Edges& m2, Eigen::MatrixXd& sdf) const ;
 
+
+    /// @todo
+    enum calcType {
+        START   = 0,
+        NORMAL  = 1,
+        END     = 2,
+    };
+    template<calcType type>
+    void singleMeshPieceSDF(const Edge& eg, const Eigen::Vector2d& tl, Eigen::MatrixXd& sdf) const;
+
+    /// @todo
+    /// @brief 计算首尾段的val 直接修改结果alpha
+    void alphaCalculation(const Edge& eg, const Eigen::Vector2d& tl, Eigen::MatrixXd& alpha, bool truc_start) const;
+
+    /// @todo
     /**
      * @brief 计算不同Grid对应的tags 以更快地确定如何进行交点计算
      */
-    void gridTagsCalculation(const Eigen::MatrixXd& sdf, MatrixXu& tags);
+    void gridTagsCalculation(const Eigen::MatrixXd& sdf, MatrixXu& tags) const;
 
     /**
      * @brief Marching Cubes的2D算法，Marching Squares
      * @note 首先需要根据SDF求出每个Grid的值，根据对应Grid选择等值线线性插值计算的位置（得到了一个个点）
      * @note 计算完插值位置之后需要顺序化
+     * @note 求出的所有边先放在一个unordered_map里面，之后按照顺序组合
      */
-    void marchingSquare(const Eigen::MatrixXd& sdf1, const Eigen::MatrixXd& sdf2, const Eigen::Vector4d& tlbr, Mesh& dst) const;
+    void marchingSquare(const Eigen::MatrixXd& sdf1, const Eigen::MatrixXd& sdf2, const Eigen::Vector4d& tlbr, EdgeMap& dst) const;
+
+private:
+    // 输入
+    /// @todo
+    void linearInterp(const Eigen::Vector4d& vals, const Vertex& pr, bool neg, Edges& dst) const;
 private:
     /**
      * 点顺序定义为： 这也是vals的(w, x, y, z) 注意激光扫描的方向是逆时针 此部分为线性插值
-     * p0->p1
-     * ^    v
-     * p3<-p2
-     */
-    static void intersectCase_0(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {}
+     * p0   边0  p1
+     * 边3       边1
+     * p3   边2  p2
+     */ 
+    // 使用8种情况表示 不同等高线情况，直接取p0恒为正数，对于不同的情况只需调换顺序
+    std::array<Vertex, 8> tb = {
+        Vertex{},
+        Vertex{{2, 3}},
+        Vertex{{1, 2}},
+        Vertex{{1, 3}},
 
-    static void intersectCase_1(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-        sp(0) = vals(3) / (std::abs(vals(2)) + std::abs(vals(3)) + 1e-5);
-        sp(1) = grid_size;
-        ep(0) = 0.0;
-        ep(1) = vals(3) / (std::abs(vals(3)) + std::abs(vals(0)) + 1e-5);
-    }
-
-    static void intersectCase_2(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_3(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_4(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_5(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_6(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_7(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_8(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_9(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_10(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_11(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-
-    static void intersectCase_12(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_13(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_14(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
-
-    static void intersectCase_15(const Eigen::Vector4d& vals, Eigen::Vector2d& sp, Eigen::Vector2d& ep) {
-
-    }
+        Vertex{{0, 1}},
+        Vertex{{0, 3}, {1, 2}},
+        Vertex{{0, 2}},
+        Vertex{{0, 3}},
+    };
 };
--- cpp/SDF/src/SDF.cc
+++ cpp/SDF/src/SDF.cc
@@ -1,20 +1,139 @@
 #include "../include/SDF.hpp"
 
-void SDF::meshBoundingBox(const Mesh& m1, const Mesh& m2, Eigen::Vector4d& tlbr) const {
-
+void SDF::meshBoundingBox(const Edges& m1, const Edges& m2, Eigen::Vector2d& tl, Eigen::Vector2i& rnc) const {
+    // 输入两个Mesh，输出Mesh对应的ROI（有裕量）的左上角点 以及纵横grid数量
 }
 
-void SDF::singleMeshSDF(const Mesh& mesh, const Eigen::Vector4d& tlbr, Eigen::MatrixXd& sdf) const {
+void SDF::singleMeshSDF(
+    const Edges& edges, const Eigen::Vector2d& tl, const Eigen::Vector2i& grid_size,
+    Eigen::MatrixXd& sdf, Eigen::MatrixXd& alpha, Eigen::MatrixXi& idx
+) const {
+    sdf = Eigen::MatrixXd(grid_size(0), grid_size(1));
+    alpha = Eigen::MatrixXd(grid_size(0), grid_size(1));
+    idx = Eigen::MatrixXi(grid_size(0), grid_size(1));
+    Eigen::MatrixXd ones = Eigen::MatrixXd(grid_size(0), grid_size(1));
+    ones.setOnes();
+    alpha.setZero();
 
-}
-
-void SDF::marchingSquare(const Eigen::MatrixXd& sdf1, const Eigen::MatrixXd& sdf2, const Eigen::Vector4d& tlbr, Mesh& dst) const {
-    Eigen::MatrixXd lut = sdf1 + sdf2;          // SDF look up table
-    int cols = lut.cols(), rows = lut.rows();
-    double sx = tlbr.w(), sy = tlbr.x(), ex = tlbr.y(), ey = tlbr.z();
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            
+    singleMeshPieceSDF<START>(edges.front(), tl, sdf);
+    size_t last_index = edges.size() - 1;
+    for (size_t i = 1; i < last_index; i++) {     // no head and tail
+        Eigen::MatrixXd tmp;
+        singleMeshPieceSDF<NORMAL>(edges[i], tl, tmp);
+        for (int j = 0; j < grid_size(0); j++) {
+            for (int k = 0; k < grid_size(1); k++) {
+                double abs_sd = std::abs(tmp(j, k));
+                if (std::abs(sdf(j, k)) > abs_sd) {
+                    idx(j, k) = i;
+                    sdf(j, k) = tmp(j, k);
+                }
+                double new_alpha = 1.0 / (abs_sd + 1.0);
+                if (alpha(j, k) < new_alpha)
+                    alpha(j, k) = new_alpha;
+            }
         }
     }
+    Eigen::MatrixXd tmp;
+    singleMeshPieceSDF<END>(edges.back(), tl, tmp);
+    for (int j = 0; j < grid_size(0); j++) {
+        for (int k = 0; k < grid_size(1); k++) {
+            double abs_sd = std::abs(tmp(j, k));
+            if (std::abs(sdf(j, k)) > abs_sd) {
+                idx(j, k) = last_index;
+                sdf(j, k) = tmp(j, k);
+            }
+        }
+    }
+    alphaCalculation(edges.front(), tl, alpha, true);
+    alphaCalculation(edges.back(), tl, alpha, false);
+}
+
+void SDF::doubleMeshSDF(const Edges& eg1, const Edges& eg2, Eigen::MatrixXd& sdf) const {
+    Eigen::Vector2d tl;
+    Eigen::Vector2i grid_size;
+    meshBoundingBox(eg1, eg2, tl, grid_size);                  // 求出两个mesh的范围 有一定的裕量
+    Eigen::MatrixXd sdf1, sdf2, alpha1, alpha2;
+    Eigen::MatrixXi idx1, idx2;
+    singleMeshSDF(eg1, tl, grid_size, sdf1, alpha1, idx1);    // 三通道输出，输出sdf，计算的权重以及到mesh哪一段距离最小
+    singleMeshSDF(eg2, tl, grid_size, sdf2, alpha2, idx2);
+    sdf = Eigen::MatrixXd(grid_size(0), grid_size(1));
+    sdf.setZero();
+    for (int i = 0; i < grid_size(0); i++) {
+        for (int j = 0; j < grid_size(1); j++) {
+            int index1 = idx1(i, j), index2 = idx2(i, j);
+            double v1 = sdf1(i, j), v2 = sdf2(i, j);
+            double w1 = alpha1(i, j) * eg1[index1].weight, w2 = alpha2(i, j) * eg2[index2].weight;
+            sdf(i, j) = (v1 * w1 + v2 * w2) / (w1 + w2);
+        }
+    }
+}
+
+template<SDF::calcType type>
+void SDF::singleMeshPieceSDF(const Edge& eg, const Eigen::Vector2d& tl, Eigen::MatrixXd& sdf) const {
+    const Eigen::Vector2d c2s = tl - eg.sp, c2e = tl - eg.sp;
+    Eigen::Vector2d direct = (eg.ep - eg.sp).normalized();
+    const Eigen::Vector2d normal = Eigen::Vector2d(-direct.y(), direct.x()).normalized();
+    for (int i = 0; i < sdf.rows(); i++) {
+        for (int j = 0; j < sdf.cols(); j++) {
+            Eigen::Vector2d p2s = c2s + Eigen::Vector2d(j, i);
+            Eigen::Vector2d p2e = c2e + Eigen::Vector2d(j, i);
+            double sdot = p2s.dot(direct), edot = p2e.dot(direct);
+            double snorm = p2s.dot(normal), enorm = p2e.norm(normal);
+            bool antis = (sdot <= 0.0), antie = (edot <= 0.0);
+            bool over_s = antis & antie, over_e = (~antis) & (~antie);
+            bool nears = (snorm > enorm), positive = (snorm > 0.0);
+            if (type == NORMAL) {
+                if (over_s) 
+                    sdf(i, j) = positive ? p2s.norm() : - p2s.norm();
+                else if (over_e)
+                    sdf(i, j) = positive ? p2e.norm() : - p2e.norm();
+                else {
+                    double dist = std::sqrt(p2s.squaredNorm() - std::pow(sdot, 2));
+                    sdf = positive ? dist : -dist;
+                }
+            }
+            else if (type == START) {
+                if (over_e)
+                    sdf(i, j) = positive ? p2e.norm() : - p2e.norm();
+                else {
+                    double dist = std::sqrt(p2s.squaredNorm() - std::pow(sdot, 2));
+                    sdf = positive ? dist : -dist;
+                }
+            }
+            else {
+                if (over_s) 
+                    sdf(i, j) = positive ? p2s.norm() : - p2s.norm();
+                else {
+                    double dist = std::sqrt(p2s.squaredNorm() - std::pow(sdot, 2));
+                    sdf = positive ? dist : -dist;
+                }
+            }
+        }
+    }
+}
+
+void SDF::marchingSquare(const Eigen::MatrixXd& sdf1, const Eigen::MatrixXd& sdf2, const Eigen::Vector4d& tlbr, EdgeMap& dst) const {
+    Eigen::MatrixXd lut = sdf1 + sdf2;          // SDF look up table
+    MatrixXu tags;
+    gridTagsCalculation(lut, tags);
+    int cols = lut.cols(), rows = lut.rows();
+    double sx = tlbr.w(), sy = tlbr.x(), ex = tlbr.y(), ey = tlbr.z();
+    for (int i = 0; i < rows - 1; i++) {
+        for (int j = 0; j < cols - 1; j++) {
+            Edges result;
+            uchar tag = tags(i, j);
+            const Vertex& vtx = tb[tag];
+            Eigen::Vector4d vals;
+            vals(0) = lut(i, j);
+            vals(1) = lut(i + 1, j);
+            vals(2) = lut(i, j + 1);
+            vals(3) = lut(i + 1, j + 1);
+            linearInterp(vals, vtx, (tag & 0x08), result);
+            std::pair<int, int> pr = {i, j};
+            for (const Edge& eg: result)                        // 保存所有边
+                dst[pr].push_back(eg);
+        }
+    }
+    /// ================= 直接可视化 ====================
+    /// @todo
 }
